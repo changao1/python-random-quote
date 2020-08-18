41aba7077275172129121d08156eb12b9255177f
stokezs <69029257+stokezs@users.noreply.github.com>
2020-08-18 21:13:46 +1000

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def most():
+def primary():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def most():
   #print(quotes)
 
 if __name__== "__main__":
-  most()
+  primary()
